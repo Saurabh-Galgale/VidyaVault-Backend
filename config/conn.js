fc0023229d9a50467fca357e5ff9b1a75568e9fa
Saurabh Galgale <110832790+Saurabh-Galgale@users.noreply.github.com>
2023-03-10 11:24:50 +0530

    changes

--- config/conn.js
+++ config/conn.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    const conn = mongoose.connect(process.env.MONGO_URI)
+    const conn = mongoose.connect(`mongodb+srv://${process.env.MONGOU}:${process.env.MONGOP}@${process.env.MONGOC}.bqgmeka.mongodb.net/?retryWrites=true&w=majority/vidyavault`)
         .then(() => {
             console.log(`MongoDB connected`);
         })
