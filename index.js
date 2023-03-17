8a71a497ed1bd5f16139359139f82d0bab058fde
Saurabh Galgale <110832790+Saurabh-Galgale@users.noreply.github.com>
2023-03-17 17:14:52 +0530

    changes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
 const cors = require("cors");
 const bodyParser = require('body-parser');
 const { connectDB } = require("./config/conn");
